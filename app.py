0d8373da0f5d3ac9d7122f518ac0654077adbddc
Muhammad Alfian <muhammadalfian500@gmail.com>
2017-09-15 05:34:52 +0700

    bismillah

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # ./app.py
 from flask import Flask
 app = Flask(__name__)
@@ -7,15 +6,6 @@ def index():
 	return "Yo, it's working!"
 if __name__ == "__main__":
 	app.run()
-=======
+
 from flask import Flask
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello():
-    return "Hello World!"
-
-if __name__ == '__main__':
-    app.run()
->>>>>>> 91611d73569c03b8d98e8cb890233f064e8b5dee
